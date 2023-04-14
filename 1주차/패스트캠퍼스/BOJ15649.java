6c4cc277bf25c4975c1ec15d32df7aa9d74df805
LeeJongbok <s2feeling@naver.com>
2023-04-14 14:08:56 +0900

    Create BOJ15649.java

---
+++ 1주차/패스트캠퍼스/BOJ15649.java
@@ -0,0 +1,48 @@
+import java.util.*; 
+
+public class Main {
+    
+    public static StringBuilder sb = new StringBuilder();
+    public static boolean[] check = new boolean[10];
+    
+    public static void recursive(ArrayList<Integer> arr, int N, int M, int cnt){
+        
+        if(M == cnt){ 
+            for(int i=0; i<arr.size(); i++){
+                sb.append(arr.get(i)+ " ");
+            }
+            sb.append('\n');
+            return; 
+        }
+        
+        
+        
+        for(int i=1; i<=N; i++){
+            if(check[i] == false){
+                check[i] = true;
+                arr.add(i);
+                recursive(arr, N, M, cnt+1);
+                check[i] = false; 
+                arr.remove(arr.size()-1);
+            }
+        }    
+    }
+    
+    
+    public static void main(String args[]) {
+      
+      Scanner sc = new Scanner(System.in);
+      
+      int N = sc.nextInt();
+      int M = sc.nextInt();
+      
+      Main T = new Main();
+      int cnt = 0;
+      ArrayList<Integer> arr = new ArrayList<>(); 
+      
+      T.recursive(arr, N, M, cnt);
+      
+      System.out.println(sb);
+      
+    }
+}
