f47ba60017639d701d9ac84e5a3f8420932d4a27
LeeJaeYun <s2feeling@naver.com>
2023-05-18 13:06:24 +0900

    Create 시각.java

---
+++ 6주차/시각.java
@@ -0,0 +1,29 @@
+import java.util.*; 
+
+public class Main {
+    public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      
+      int N = sc.nextInt();
+      int ans = 0; 
+      
+      for(int i=0; i<=N; i++){
+          for(int j=0; j<=59; j++){
+              for(int k=0; k<=59; k++){
+                String tmp = Integer.toString(i) + Integer.toString(j) + Integer.toString(k);
+                for(int l=0; l<tmp.length(); l++){
+                    if(tmp.charAt(l) == '3'){
+                        ans++;
+                        break;
+                    }
+                }
+                
+              }
+          }
+      }
+          
+      System.out.println(ans);       
+    }
+      
+     
+}
