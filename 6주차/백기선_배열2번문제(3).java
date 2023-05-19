98fcb7ded65444d041b91ed305f3fca6cc0766c8
LeeJaeYun <s2feeling@naver.com>
2023-05-19 16:28:34 +0900

    Create 백기선_배열2번문제(3).java

---
+++ 6주차/백기선_배열2번문제(3).java
@@ -0,0 +1,45 @@
+// 시간 복잡도: O(N)
+// 공간 복잡도: O(N)
+
+// 왜 맞았는가?
+// 1. swap을 구현해서, 맨 앞과 맨 뒤를 변경해주었다.
+// 2. 이 때, swap 메소드에는 배열과, 시작 위치, 끝 위치, 3개의 변수를 같이 전달해주어야 한다. 
+// -> 그래야만, 값이 제대로 바뀔 수 있다. 
+
+import java.util.*; 
+
+public class Main {
+    
+    
+    public static void swap(char[] arr, int start, int end){
+        
+        char tmp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = tmp;
+    }
+    
+    
+    public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      
+      String str = sc.nextLine();
+      char[] arr = str.toCharArray();
+      
+      int start = 0;
+      int end = arr.length-1;
+      
+      while(start < end){
+          swap(arr, start, end);
+          start++;
+          end--;
+      }
+      
+      for(int i=0; i<arr.length; i++){
+          System.out.print(arr[i]+ " ");
+      }
+      
+      
+      
+      
+    }
+}
